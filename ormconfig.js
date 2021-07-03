21895a2f6ac1f8971340853c6fada1c9a58a51e9
Sohail <m_sohail10@live.com>
2021-07-03 09:27:44 +0500

    db host from env

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: "mysql",
-  host: "localhost",
+  host: process.env.DATABASE_HOST,
   port: 3306,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
