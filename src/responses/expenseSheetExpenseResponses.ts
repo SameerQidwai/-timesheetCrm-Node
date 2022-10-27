67170ccd0295312d29ae01ee07bc6e4768f5b1b4
Shahzaib Ahmed <shahzaib@akvateq.com>
2022-10-27 16:41:49 +0500

    Expense sheet expense dto

---
+++ src/responses/expenseSheetExpenseResponses.ts
@@ -0,0 +1,38 @@
+import { ExpenseSheetExpense } from '../entities/expenseSheetExpense';
+import { Expense } from '../entities/expense';
+
+export class ExpenseSheetExpenseResponse {
+  id: number;
+  expenseTypeId: number;
+  expenseTypeName: String;
+  date: Date;
+  projectId: number | null;
+  projectName: String | null;
+  amount: number;
+  isBillable: Boolean;
+  isReimbursed: Boolean;
+  notes: string | null;
+
+  constructor(expense: ExpenseSheetExpense) {
+    this.id = expense.id;
+    this.expenseTypeId = expense.expense.expenseTypeId;
+    this.expenseTypeName = expense.expense.expenseType.label;
+    this.date = expense.expense.date;
+    this.projectId = expense.expense.projectId;
+    this.projectName = expense.expense.project?.title;
+    this.amount = expense.expense.amount;
+    this.isBillable = expense.expense.isBillable ? true : false;
+    this.isReimbursed = expense.expense.isReimbursed ? true : false;
+    this.notes = expense.expense.notes;
+  }
+}
+
+export class ExpenseSheetExpensesResponse {
+  expenses: ExpenseSheetExpenseResponse[] = [];
+
+  constructor(expenses: ExpenseSheetExpense[]) {
+    expenses.forEach((expense) => {
+      this.expenses.push(new ExpenseSheetExpenseResponse(expense));
+    });
+  }
+}
