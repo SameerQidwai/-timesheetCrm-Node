302d7ab051836cc10bcd3b8b504f523272d28733
Shahzaib Ahmed <shahzaib@akvateq.com>
2022-12-16 17:31:06 +0500

    mailer env disable

--- src/utilities/mailer.ts
+++ src/utilities/mailer.ts
@@ -38,14 +38,16 @@ export let sendMail = (
     html: html,
   };
 
-  transporter.sendMail(mail, (err, info) => {
-    if (err) {
-      console.log('Error occurred. ' + err.message);
-      return process.exit(1);
-    }
+  if (process.env.SEND_EMAIL === 'true') {
+    transporter.sendMail(mail, (err, info) => {
+      if (err) {
+        console.log('Error occurred. ' + err.message);
+        return process.exit(1);
+      }
 
-    console.log('Message sent: %s', info.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  });
+      console.log('Message sent: %s', info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+  }
 };
