31878fe7825548cb2a88bd013c9ed0bc8927c46a
Sameer Qidwai <samiqidwai@gmail.com>
2022-12-26 22:41:09 +0500

    push

--- src/entities/views/profitView.ts
+++ src/entities/views/profitView.ts
@@ -1,8 +1,8 @@
 import { ViewEntity, ViewColumn } from "typeorm"
 
 @ViewEntity({
-    name: 'Profit_View',
-    expression: `SELECT project_id, resource_view.milestone_id, resource_start, 
+    name: 'profit_view',
+    expression: `SELECT project_id, Resource_View.milestone_id, resource_start, 
         resource_end, resource_buying_rate, resource_selling_rate, 
         resource_contact_person_id, resource_employee_id, resource_name, 
             project_organization_id, project_organization_name, 
@@ -10,15 +10,15 @@ import { ViewEntity, ViewColumn } from "typeorm"
         project_phase, project_title, time_entries_view.entry_date, time_entries_view.entry_id, 
         time_entries_view.actual_hours, project_start, project_end
 
-        FROM resource_view
+        FROM Resource_View
             LEFT JOIN time_entries_view
                 ON resource_employee_id = time_entries_view.employee_id
-                AND resource_view.milestone_id = time_entries_view.milestone_id
+                AND Resource_View.milestone_id = time_entries_view.milestone_id
                 AND STR_TO_DATE(time_entries_view.entry_date,'%e-%m-%Y') BETWEEN STR_TO_DATE(DATE_FORMAT(resource_start,'%e-%m-%Y'),'%e-%m-%Y')  
                 AND resource_end`
     })
         // AND STR_TO_DATE(time_entries_view.entry_date,'%e-%m-%Y') 
-        //     BETWEEN STR_TO_DATE(DATE_FORMAT(res_start,'%e-%m-%Y'),'%e-%m-%Y') AND resource_view.res_end;
+        //     BETWEEN STR_TO_DATE(DATE_FORMAT(res_start,'%e-%m-%Y'),'%e-%m-%Y') AND Resource_View.res_end;
 export class ProfitView {
 
     @ViewColumn()
