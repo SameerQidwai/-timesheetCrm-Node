348bea855cc43faf04d7e7e7784fa62452c1fe1f
Shahzaib Ahmed <thedevshezzy@gmail.com>
2023-02-25 17:34:59 +0500

    test mail route

--- src/controllers/sampleController.ts
+++ src/controllers/sampleController.ts
@@ -1,7 +1,8 @@
-import { Sample } from "src/entities/sample";
-import { SampleRepository } from "src/repositories/sampleRepository";
-import { BaseController } from "./baseController";
+import { Sample } from 'src/entities/sample';
+import { SampleRepository } from 'src/repositories/sampleRepository';
+import { BaseController } from './baseController';
 
-export class SampleController extends BaseController<Sample, SampleRepository> {
-    
-}
+export class SampleController extends BaseController<
+  Sample,
+  SampleRepository
+> {}
