478e1cb394749fb857bc1a11a8327c01fe678e6d
Shahzaib Ahmed <shahzaib@akvateq.com>
2023-08-08 22:33:47 +0500

    timesheet fix script

---
+++ src/timesheetEndDateFixScript.ts
@@ -0,0 +1,35 @@
+import moment from 'moment';
+import {
+  createConnection,
+  EntityManager,
+  getManager,
+  IsNull,
+  Not,
+} from 'typeorm';
+import { LeaveRequest } from './entities/leaveRequest';
+import { LeaveRequestBalance } from './entities/leaveRequestBalance';
+import { LeaveRequestPolicyLeaveRequestType } from './entities/leaveRequestPolicyLeaveRequestType';
+import { Timesheet } from './entities/timesheet';
+const connection = createConnection();
+
+connection
+  .then(async () => {
+    const manager = getManager();
+
+    await manager.transaction(async (trx: EntityManager) => {
+      let timesheets = await trx.find(Timesheet, {});
+      for (let timesheet of timesheets) {
+        timesheet.endDate = moment(timesheet.endDate, 'YYYY-MM-DD HH:mm:ss:SSS')
+          .endOf('day')
+          .toDate();
+        await trx.save(timesheet);
+      }
+    });
+
+    console.log('timesheets updated');
+
+    return true;
+  })
+  .catch((error) => {
+    console.error('error in DB connection: ', error);
+  });
